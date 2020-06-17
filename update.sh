f3f549afd71f6d31760118c67d18a12ec8e97342
Tom Brown <tom@covidactnow.org>
2020-06-17 11:57:12 -0700

    rm interventions-naco (#64)
    The NaCo dataset actually predates the recent interventions effort.  we started pulling it in when we were hoping to get county-level stay-at-home orders, but that became irrelevant.
    
    See https://covidactnow.slack.com/archives/C010PNQM82G/p1592412149107800

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ set -o errexit
 python scripts/update.py
 python scripts/update_covid_tracking_data.py
 python scripts/update_covid_care_map.py
-python scripts/update_interventions_naco.py
 python scripts/update_nha_hospitalization_county.py
 python scripts/update_nytimes_data.py
 python scripts/update_test_and_trace.py
