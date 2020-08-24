d0c9b1251949e84eaf45a38814a7bb579181f222
tashwoods <tashwoods@gmail.com>
2020-08-24 16:23:18 -0600

    update data 8/24

--- forecast_data/update_forecast_data.py
+++ forecast_data/update_forecast_data.py
@@ -9,9 +9,11 @@ def get_data(signal, datasource):
 
 
 if __name__ == "__main__":
-    # signals = {"full_time_work_prop": "safegraph", "part_time_work_prop":"safegraph", "median_home_dwell_time": "safegraph", "completely_home_prop": "safegraph",
-    signals = {"smoothed_cli": "doctor-visits", "smoothed_adj_cli": "doctor-visits"}
+    # signals = {"full_time_work_prop": "safegraph", "part_time_work_prop":"safegraph", "median_home_dwell_time": "safegraph", "completely_home_prop": "safegraph","smoothed_cli": "doctor-visits", "smoothed_adj_cli": "doctor-visits"}
+    signals = {"median_home_dwell_time": "safegraph"}
     for signal, datasource in signals.items():
         get_data(signal, datasource)
+
+    print("saved data")
     # with Pool(processes = 4) as p:
     #  p.map(get_data(signals))
